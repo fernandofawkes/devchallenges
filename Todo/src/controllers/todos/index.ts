90a6652975f8836747a8b664438f1c9fa983c81f
Fernando Malaman <fernandomalaman@brq.com>
2020-07-21 20:19:03 -0300

    add deleteTodo method

--- Todo/src/controllers/todos/index.ts
+++ Todo/src/controllers/todos/index.ts
@@ -1,46 +1,46 @@
 import { Request, Response } from "express";
 import { ITodo } from "../../types/todo";
-import  Todo from "../../models/todo";
+import Todo from "../../models/todo";
 
 const getTodos = async (req: Request, res: Response): Promise<void> => {
   try {
     const todos: ITodo[] = await Todo.find();
-    res.status(200).json({todos});
-  } catch(err) {
+    res.status(200).json({ todos });
+  } catch (err) {
     throw err;
   }
 }
 
 const addTodo = async (req: Request, res: Response): Promise<void> => {
   try {
-      // used typecasting to avoid adding fields from the Document class fron mongoose
-      const body = req.body as Pick<ITodo, "name"|"description"|"status">;
-      const {name, description, status} = body;
-      
-      const todo = new Todo({
-        name,
-        description,
-        status
-      });
+    // used typecasting to avoid adding fields from the Document class fron mongoose
+    const body = req.body as Pick<ITodo, "name" | "description" | "status">;
+    const { name, description, status } = body;
 
-      const newTodo = await todo.save();
-      // get updated todos array
-      const allTodos = await Todo.find();
+    const todo = new Todo({
+      name,
+      description,
+      status
+    });
 
-      res.status(201).json({
-        message: "todo sucessfully addded",
-        todo:newTodo,
-        todos: allTodos
-      });
-  } catch(err) {
+    const newTodo = await todo.save();
+    // get updated todos array
+    const allTodos = await Todo.find();
+
+    res.status(201).json({
+      message: "todo sucessfully addded",
+      todo: newTodo,
+      todos: allTodos
+    });
+  } catch (err) {
     throw err
   };
 }
 
-const updateTodo = async(req: Request, res: Response): Promise<void> => {
+const updateTodo = async (req: Request, res: Response): Promise<void> => {
   try {
     // object destructuring to get needded attributes
-    const {params: {id}, body } = req;
+    const { params: { id }, body } = req;
 
     const updatedTodo = await Todo.findByIdAndUpdate({
       _id: id
@@ -54,7 +54,29 @@ const updateTodo = async(req: Request, res: Response): Promise<void> => {
       todo: updatedTodo,
       todos: allTodos
     })
-  }catch(err){
+  } catch (err) {
+    throw err
+  }
+}
+
+const deleteTodo = async (req: Request, res: Response): Promise<void> => {
+  try {
+    // object destructuring to get needded attributes
+    const { params: { id } } = req;
+
+    const deleted = await Todo.findByIdAndDelete({
+      _id: id
+    });
+
+    // get updated todos array
+    const allTodos = await Todo.find();
+
+    res.status(200).json({
+      message: "todo sucessfully deleted",
+      todo: deleted,
+      todos: allTodos
+    })
+  } catch (err) {
     throw err
   }
 }
