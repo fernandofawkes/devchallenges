10bc3c65103bb22c3beab04b4ec393c09ae67129
Fernando Malaman <fernandomalaman@brq.com>
2020-07-21 19:57:55 -0300

    add todo model

---
+++ Todo/src/models/todo.ts
@@ -0,0 +1,19 @@
+import {ITodo} from '../types/todo';
+import { Schema, model } from 'mongoose';
+
+const todoSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: Boolean,
+    required: true
+  },
+}, {timestamps: true});
+
+export default model<ITodo>("Todo", todoSchema);
