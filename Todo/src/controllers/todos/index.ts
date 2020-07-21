f7f34fcc728f26647dc8c40eca3cc83b5162bd0d
Fernando Malaman <fernandomalaman@brq.com>
2020-07-21 20:12:39 -0300

    add  addTodo method

--- Todo/src/controllers/todos/index.ts
+++ Todo/src/controllers/todos/index.ts
@@ -10,3 +10,29 @@ const getTodos = async (req: Request, res: Response): Promise<void> => {
     throw err;
   }
 }
+
+const addTodo = async (req: Request, res: Response): Promise<void> => {
+  try {
+      // used typecasting to avoid adding fields from the Document class fron mongoose
+      const body = req.body as Pick<ITodo, "name"|"description"|"status">;
+      const {name, description, status} = body;
+      
+      const todo = new Todo({
+        name,
+        description,
+        status
+      });
+
+      const newTodo = await todo.save();
+      // get updated todos array
+      const allTodos = await Todo.find();
+
+      res.status(201).json({
+        message: "todo sucessfully addded",
+        todo:newTodo,
+        todos: allTodos
+      });
+  } catch(err) {
+    throw err
+  };
+}
