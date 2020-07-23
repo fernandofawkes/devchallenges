50834f7f57b9aad47c0aedaa1439ba2068c6c1c2
Fernando Malaman <fernandomalaman@brq.com>
2020-07-22 22:21:47 -0300

    scaffold main component

--- Todo/client/src/App.tsx
+++ Todo/client/src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import AddTodo from './components/AddTodo';
+import Todo from './components/Todo';
+import { getTodos } from './API';
 
-function App() {
+const App: React.FC = () =>  {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  // useEffect with no dependencies run on mount and dismisses on unmount 
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = () => {
+    getTodos().then(({ data: { todos } }: ITodo[] | any) => setTodos(todos))
+    .catch((err: Error) => console.log(err));
+  };
+
   return (
-    <div className="App">
-     
-    </div>
+    <main className="App">
+    <AddTodo saveTodo={() => {}} ></AddTodo>
+    {
+      todos.map(todo => <Todo key={todo._id} todo={todo} updateTodo={() => {}} deleteTodo={() => {}}></Todo>)
+    }
+    </main>
   );
 }
 
